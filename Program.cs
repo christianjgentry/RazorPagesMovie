2ca881714545a2333728a3df10eb3e1b835726d0
christianjgentry <32580981+christianjgentry@users.noreply.github.com>
2019-11-10 20:49:44 -0600

    Cleaned up

--- Program.cs
+++ Program.cs
@@ -1,17 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Models;
-
-using System.Net.Http;
-using System.Net.Http.Headers;
-
 
 namespace RazorPagesMovie
 {
